2853ad5b1c7f181cb37a328808b8fb054a5237bb
zenn <zeinoarkananta@gmail.com>
2025-12-30 14:27:33 +0700

    tpdb

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -13,7 +13,7 @@
              
             <img src="https://images.unsplash.com/photo-1604335399105-a0c585fd81a1?q=80&w=2070&auto=format&fit=crop" 
                  loading="lazy"
-                 class="absolute inset-0 w-full h-full object-cover opacity-30 mix-blend-overlay transition-transform duration-1000 group-hover:scale-110">
+                 alt="image background" class="absolute inset-0 w-full h-full object-cover opacity-30 mix-blend-overlay transition-transform duration-1000 group-hover:scale-110">
             <div class="absolute inset-0 bg-gradient-to-r from-brand-900/90 to-brand-600/40"></div>
             
             <div class="relative z-10 p-6 md:p-12 h-full flex flex-col justify-center items-start text-white">
