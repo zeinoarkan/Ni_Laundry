7adb4bb020a42a6db65f0a56ac8e02e7388eaece
zenn <zeinoarkananta@gmail.com>
2026-01-01 10:01:34 +0700

    tpdb

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Pelanggan;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
